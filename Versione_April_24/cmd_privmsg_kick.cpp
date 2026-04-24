5cef957562e6a7dbd327cf0471a3b42e93f31935
correcarolina <correcarolina@gmail.com>
2026-04-24 13:22:06 +0200

    no comment

---
+++ Versione_April_24/cmd_privmsg_kick.cpp
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_privmsg_kick.cpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cacorrea <cacorrea@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/24 11:50:22 by cacorrea          #+#    #+#             */
+/*   Updated: 2026/04/24 12:50:04 by cacorrea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "server.hpp"
+
+//command use: PRIVMSG <receiver> <text to be sent>
+void Server::execPrivmsg(Client& client, const std::string& params)
+{
+    if (params.empty()) {
+        sendReply(client, "411 :No recipient given\r\n");
+        return;
+    }
+    size_t spacePos = params.find(' '); 
+    if (spacePos == std::string::npos) {
+        sendReply(client, ":" + getName() + " 411 " + client.getNick() + " :No recipient given\r\n");
+        return;
+    }
+    std::string target = params.substr(0, spacePos);
+    if (target.empty()) {
+        sendReply(client, ":" + getName() + " 411 " + client.getNick() + " :No recipient given\r\n");
+        return;
+    }
+    std::string message = params.substr(spacePos + 1);
+    if (message.empty()) {
+        sendReply(client, ":" + getName() + " 412 " + client.getNick() + " :No text to send\r\n");
+        return;
+    }
+    if (message[0] == ':')
+        message = message.substr(1);
+    else {
+        sendReply(client, "412 :No text to send\r\n");
+        return;
+    }
+    std::string fullMsg = ":" + client.getPrefix() + " PRIVMSG " + target + " :" + message;
+    if (target[0] == '#') { // Caso canale
+        std::map<std::string, Channel>::iterator it = channels.find(target);
+        if (it == channels.end()) {
+            sendReply(client, ":" + getName() + " 401 " + client.getNick() + " " + target + " :No such nick/channel\r\n");
+            return;
+        }
+        if (!it->second.isMember(client)) {
+            sendReply(client, ":" + getName() + " 404 " + client.getNick() + " " + target + " :Cannot send to channel\r\n");
+            return;
+        }
+        it->second.broadcast(fullMsg, &client);
+        return;
+    }
+    for (size_t i = 0; i < client_vect.size(); ++i) {
+        if (client_vect[i].getNick() == target) {
+            client_vect[i].getWriteBuffer() += fullMsg + "\r\n";
+            poll_fds[i].events |= POLLOUT;
+            return;
+        }
+    }
+    sendReply(client, ":" + getName() + " 401 " + client.getNick() + " " + target + " :No such nick/channel\r\n");
+}
+
+void Server::execKick(Client& client, const std::string& params){
+	if (params.empty()){
+		sendReply(client, "461 KICK :Not enough parameters\r\n");
+        return ;
+	}
+	size_t spacePos = params.find(' ');
+    if (spacePos == std::string::npos){
+        sendReply(client, "461 KICK :Not enough parameters\r\n");
+        return ;
+    }
+	std::string channelName = params.substr(0, spacePos);
+    std::string rest = params.substr(spacePos + 1);
+	size_t spacePos2 = rest.find(' ');
+    std::string targetNick;
+    std::string reason;
+	if (spacePos2 == std::string::npos){
+		targetNick = rest;
+		reason = client.getNick();
+	}
+	else{
+		targetNick = rest.substr(0, spacePos2);
+		std::string reasonPart = rest.substr(spacePos2 + 1);
+		if (!reasonPart.empty() && reasonPart[0] == ':')
+			reason = reasonPart.substr(1);
+		else
+			reason = client.getNick();
+	}
+	std::map<std::string, Channel>::iterator it = channels.find(channelName);
+    if (it == channels.end()){
+        sendReply(client, "403 " + channelName + " :No such channel\r\n");
+        return ;
+    }
+    Channel& channel = it->second;
+	if (!channel.isMember(client)){
+        sendReply(client, "442 " + channelName + " :You're not on that channel\r\n");
+        return ;
+	}
+	if (!channel.isOperator(client)){
+        sendReply(client, "482 " + channelName + " :You're not a channel operator\r\n");
+        return ;
+	}
+	Client* targetClient = NULL;
+	for (size_t i = 1; i < client_vect.size(); i++){
+		if (client_vect[i].getNick() == targetNick){
+			targetClient = &client_vect[i];
+			break ;
+		}
+	}
+	if (!targetClient){
+		sendReply(client, "401 " + targetNick + " :No such nick\r\n");
+        return ;
+	}
+	if (!channel.isMember(*targetClient)){
+        sendReply(client, "441 " + targetNick + " " + channelName + " :They aren't on that channel\r\n");
+        return ;
+	}
+	std::string kickMsg = ":" + client.getPrefix() + " KICK " + channelName + " " + targetNick + " :" + reason + "\r\n";
+	channel.broadcast(kickMsg, NULL);
+	channel.removeClient(*targetClient);
+}
---
+++ Versione_April_24/ircbot.hpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ircbot.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vloddo <vloddo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/23 13:00:27 by sel-khao          #+#    #+#             */
+/*   Updated: 2026/04/23 19:48:45 by vloddo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IRCBOT_HPP
+# define IRCBOT_HPP
+
+#include <fcntl.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <cstring>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+class IRCBot {
+    private:
+        int                 sock_fd;
+        std::string         server_ip;
+        int                 server_port;
+        std::string         password;
+        std::string         nickname;
+        std::string         username;
+        std::string         channel;
+        std::string         read_buffer;
+        std::vector<std::string> quotes;
+        std::string         sender;
+
+        void                setupQuotes(void);
+        void                sendRaw(const std::string& msg);
+        std::string         receiveLine(void);
+		void                registerToServer(void);
+		void                joinChannel(void);
+		void                sendHelp();
+		void                processCommand(const std::string& sender, const std::string& target, const std::string& cmd, const std::string& args);
+		void                parseMessage(const std::string& line);
+public:
+	IRCBot(const std::string& ip, int port, const std::string& pass, const std::string& nick, const std::string& chan);
+	~IRCBot(void);
+
+	void                connectToServer(void);
+	void                run(void);
+};
+
+#endif
